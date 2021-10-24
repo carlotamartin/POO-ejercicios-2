3ad2d9150897fcf35bf1a467d5b5484b306d98cd
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2021-10-24 20:06:28 +0200

    Más cambios

---
+++ ClassModelo.py
@@ -0,0 +1,19 @@
+# El modelo se encarga de la gestión de los datos.
+class Modelo:
+    def __init__(self):
+        self.cadenas = []
+        # Añade una cadena a la lista.
+    def agregar(self, cadena):
+        self.cadenas.append(cadena)
+        # Devuelve la lista de cadenas.
+    def recuperarCadenas():
+    return self.cadenas
+    # Los actores MVC son globales en este ejemplo...
+    vista = Vista ()
+    controlador = Controlador()
+    modelo = Modelo()
+    # Vamos a recuperar dos líneas de la entrada estándar.
+    for _ in range(2):
+        controlador.almacenarEntrada()
+        # Después las guardamos en un archivo.
+        controlador.guardarCadenas()
